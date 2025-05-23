c8635b1294ca2b0eda1b258a00d18dea09d79b40
kirk2242 <kirklhoel.labay@gmail.com>
2025-05-23 09:53:05 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -89,15 +89,26 @@ CREATE TABLE sales_items (
 
 */
 
+
+// online website database
+
 // $host = "localhost"; // Change if necessary
 // $dbname = "u866427573_time"; // Ensure this matches your actual database name
 // $username = "u866427573_time"; // Default username for XAMPP
 // $password = "D6am|y9G>"; // Default password for XAMPP (empty)
 
+
+
+
+// localhost database
+
  $host = "localhost"; // Change if necessary
  $dbname = "Time_Emporium_DB"; // Ensure this matches your actual database name
  $username = "root"; // Default username for XAMPP
  $password = ""; // Default password for XAMPP (empty)
+
+
+
 
 // Establish PDO connection
 try {
